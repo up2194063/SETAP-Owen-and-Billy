a31d805f0ed75658e1275e448b97bccced6869c5
up2194063 <up2194063@myport.ac.uk>
2025-03-31 10:27:22 +0100

    Create tasks.py

---
+++ hsa/tasks.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
+from werkzeug.exceptions import abort
+
+from .db import get_db
+
+bp = Blueprint("tasks", __name__)
